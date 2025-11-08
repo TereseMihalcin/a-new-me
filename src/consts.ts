a647e802fc236be8b91a44e2d126b9191bf23cd0
Terese Mihalcin <teresemihalcin18@gmail.com>
2025-11-07 19:06:40 -0500

    site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Terese Mihalcin';
+export const SITE_TITLE = 'Terese Mihalcin Blog';
 export const SITE_DESCRIPTION = 'My blog about stuff and things';
